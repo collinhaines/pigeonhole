8df879df4a771a57016c283b2c6099b44c6a8f12
Collin Haines <collinhaines@me.com>
2016-08-29 17:19:04 -0400

    Gave up on Actual Unit Testing

---
+++ imports/ui/layouts/client/something.test.js
@@ -0,0 +1,10 @@
+// One of these days I'll get around to doing actual testing..
+// Not like it's needed for a business card website.
+
+import { chai } from 'meteor/practicalmeteor:chai';
+
+describe('testing', function () {
+  it('something', function () {
+    chai.assert.equal(0, 0);
+  });
+});
