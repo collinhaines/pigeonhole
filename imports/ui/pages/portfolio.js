518e3df21ab2261b5672318e6874ee2c765580f0
Collin Haines <collinhaines@me.com>
2016-09-23 22:18:36 -0400

    Basic .list-filter Layout

--- imports/ui/pages/portfolio.js
+++ imports/ui/pages/portfolio.js
@@ -1,1 +1,15 @@
 import './portfolio.html';
+
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+
+Template.portfolio.events({
+  'click #filter a'(event) {
+    event.target.blur();
+    event.preventDefault();
+
+    $('#filter a.current').removeClass('current');
+
+    $(event.target).addClass('current');
+  }
+});
