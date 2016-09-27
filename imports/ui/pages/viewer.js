50bbae6eca4c98b0260e0c8a59b472778d6913ba
Collin Haines <collinhaines@me.com>
2016-09-27 11:04:34 -0400

    Added Keys: endDate, startDate - Removed Key: date

--- imports/ui/pages/viewer.js
+++ imports/ui/pages/viewer.js
@@ -32,5 +32,29 @@ Template.viewer.helpers({
 
   isMobileType(type) {
     return type === 'mobile';
+  },
+
+  renderDate(start, end) {
+    const getDate = (date) => {
+      // This statement is disgusting to look at.
+      return {
+        0: 'January',
+        1: 'February',
+        2: 'March',
+        3: 'April',
+        4: 'May',
+        5: 'June',
+        6: 'July',
+        7: 'August',
+        8: 'September',
+        9: 'October',
+        10: 'November',
+        11: 'December'
+      }[date.getUTCMonth()] + ' ' + date.getUTCFullYear();
+    };
+
+    return end === '' ?
+      getDate(new Date(start)) + ' - Current' :
+      getDate(new Date(start)) + ' - ' + getDate(new Date(end));
   }
 });
