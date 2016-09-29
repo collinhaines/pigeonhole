2767e5ca74120f1ece877dd53b296d614bb9708f
Collin Haines <collinhaines@me.com>
2016-09-29 09:44:10 -0400

    Filter Visual Effects

--- imports/ui/pages/portfolio.js
+++ imports/ui/pages/portfolio.js
@@ -32,8 +32,22 @@ Template.portfolio.events({
     event.target.blur();
     event.preventDefault();
 
+    // Switch visual representation of current filter.
     $('#filter a.current').removeClass('current');
 
     $(event.target).addClass('current');
+
+    const type = $(event.target).data('filter');
+
+    // Fade in and out proper items.
+    if (type === 'all') {
+      $('.portfolio-content .row .col-sm-4').fadeIn();
+    } else {
+      $('.portfolio-content .row .col-sm-4')
+        .not('[data-type="' + type + '"]')
+        .fadeOut();
+
+      $('.portfolio-content .row .col-sm-4[data-type="' + type + '"]').fadeIn();
+    }
   }
 });
